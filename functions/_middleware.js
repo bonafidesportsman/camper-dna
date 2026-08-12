dc55e195a911c6970ad27bb702153f7d762ccc7b
Guy <bonafidesportsman@users.noreply.github.com>
2026-08-12 10:09:27 +0100

    Rewrite clean CamperDNA URLs in middleware

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -97,8 +97,15 @@ async function routeOr404(request, next) {
   const url = new URL(request.url);
   const pathname = normalizePathname(url.pathname);
 
+  const resolvedRoute = resolveKnownRoute(pathname);
+  if (resolvedRoute) {
+    if (resolvedRoute === pathname) return next();
+    const rewrittenUrl = new URL(request.url);
+    rewrittenUrl.pathname = resolvedRoute;
+    return next(new Request(rewrittenUrl, request));
+  }
+
   if (
-    isKnownRoute(pathname) ||
     PASSTHROUGH_FILES.has(pathname) ||
     PASSTHROUGH_PREFIXES.some(prefix => pathname.startsWith(prefix))
   ) {
@@ -133,12 +140,15 @@ async function routeOr404(request, next) {
 }
 
 
-function isKnownRoute(pathname) {
-  if (VALID_ROUTES.has(pathname)) return true;
+function resolveKnownRoute(pathname) {
+  if (VALID_ROUTES.has(pathname)) return pathname;
   if (pathname.endsWith('/')) {
-    return VALID_ROUTES.has(`${pathname.slice(0, -1)}.html`);
+    const htmlRoute = `${pathname.slice(0, -1)}.html`;
+    if (VALID_ROUTES.has(htmlRoute)) return htmlRoute;
   }
-  return VALID_ROUTES.has(`${pathname}.html`);
+  const htmlRoute = `${pathname}.html`;
+  if (VALID_ROUTES.has(htmlRoute)) return htmlRoute;
+  return '';
 }
 
 function normalizePathname(pathname) {
